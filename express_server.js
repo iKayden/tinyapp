26d260b656fda946f756309bab034f2654878067
ikayden <ikyden23@gmail.com>
2022-10-03 15:12:14 -0700

    Add views dir and path for urls

--- express_server.js
+++ express_server.js
@@ -13,8 +13,13 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/hello", (req, res) => {
-  res.send("<hrml><body>Hello <b>World!</b></body></hrml>\n");
+  res.send("<html><body>Hello <b>World!</b></body></html>\n");
 });
 
 app.get("/", (req, res) => {
