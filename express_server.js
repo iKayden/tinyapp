030c236987b01a8c0a226357ef9e601a98462c97
ikayden <ikyden23@gmail.com>
2022-10-03 20:27:18 -0700

    Added form for new submition and new get route for it

--- express_server.js
+++ express_server.js
@@ -2,13 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// settings for the Express server
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
+//our mock-data-base
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+// Routes for the server
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -35,6 +43,7 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// -------- Server Initiation ------------------
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
